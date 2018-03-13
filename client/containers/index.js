3db458dc4691d0e890fa5fe9a60266d743b24c37
Guillermo Saavedra <gs22525@email.vccs.edu>
2018-03-13 10:53:07 -0400

    heroku

--- client/containers/index.js
+++ client/containers/index.js
@@ -1,5 +1,5 @@
 export { default as AutoCompleteList } from './AutoCompleteList';
-// export { default as Button } from './Button';
+export { default as Button } from './Button';
 export { default as JobList } from './JobList';
 export { default as JobTitleSearchBar } from './JobTitleSearchBar';
 export { default as NavigationFooter } from './NavigationFooter';
