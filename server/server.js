157b4c9a679d4a77ac4bfd08053eb883cedea8a4
Guillermo Saavedra <gs22525@email.vccs.edu>
2018-03-25 13:06:23 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -12,27 +12,26 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// const webpackMiddleware = require('webpack-dev-middleware');
-// const webpack = require('webpack');
-// const webpackConfig = require('../webpack.config.js');
-
-// app.use(webpackMiddleware(webpack(webpackConfig)));
-
+if (process.env.NODE_ENV=='development') {
+	console.log('development set up');
+	const webpackMiddleware = require('webpack-dev-middleware');
+	const webpack = require('webpack');
+	const webpackConfig = require('../webpack.config.js');
+	app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+	console.log('production set up');
+	app.use(express.static('./'));
+	app.get('*',(req, res)=>{
+  		res.sendFile(path.resolve(__dirname,'../index.html'));
+	});
+}
 app.use('/graphql',expressGraphQL({
 	schema,
 	graphiql: true
 }));
-
-app.use(express.static('./'));
-app.get('*',(req, res)=>{
-  res.sendFile(path.resolve(__dirname,'../index.html'));
-});
-console.log(process.env.SUGGESTIONS_DB);
-mongoose.connect(process.env.SUGGESTIONS_DB);
+mongoose.connect(process.env.MLAB_DB_URL);
 mongoose.connection
-	.once('open',()=>console.log('Connec1111ted to MongoDB'))
+	.once('open',()=>console.log('Connected to MongoDB'))
 	.on('error',()=>console.log('Error when connecting to database'));
-
-
 
 module.exports = app;
